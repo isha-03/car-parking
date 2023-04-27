4a9d45d6d9ffe310811e78438b8ef2b66dae6463
isha-03 <ijain2_be21@thapar.edu>
2023-04-27 22:46:59 +0530

    linked spots.html with cards

--- src/Pages/Locations/Components/Cards.js
+++ src/Pages/Locations/Components/Cards.js
@@ -9,7 +9,7 @@ export default function Cards() {
   <div className="card-body">
     <h5 className="card-title">Main Gate</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-outline-success"><Link to="/spot">Check</Link></a>
+    <a href="./spots.html" className="btn btn-outline-success">Check</a>
   </div>
 </div>
 <div className="card mx-3 text-bg-dark p-3" style={{width: 250}}>
